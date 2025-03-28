f19772b81f8f330e3d7a0b60d94b9fb2159a66bc
Greg Martinez <greg-martinez44@outlook.com>
2025-03-28 14:49:04 -0700

    adjust path to files in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -l
 
-pip install -r requirements.txt
+python -m pip install --upgrade pip
 
-FKGL=$(python check_readability.py -d $1 --stat flesch_kincaid_grade)
-FRE=$(python check_readability.py -d $1 --stat flesch_reading_ease)
+pip install -r /requirements.txt
+
+FKGL=$(python /check_readability.py -d $1 --stat flesch_kincaid_grade)
+FRE=$(python /check_readability.py -d $1 --stat flesch_reading_ease)
 
 echo "FKGL=$FKGL" >> $GITHUB_OUTPUT
 echo "FRE=$FRE" >> $GITHUB_OUTPUT
